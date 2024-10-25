10e81aac3083f705a5cabdcc3e31d82b9aa41fbf
ElRoman7 <sergio.romam@outlook.com>
2024-10-25 11:19:31 -0600

    Antes de Insertar por Lote

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,16 +4,17 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { join } from 'path';
 import { PokemonModule } from './pokemon/pokemon.module';
 import { CommonModule } from './common/common.module';
+import { SeedModule } from './seed/seed.module';
 
 @Module({
   imports: [
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
     }),
-
     MongooseModule.forRoot('mongodb://localhost:27018/nest-pokemon'),
     PokemonModule,
     CommonModule,
+    SeedModule,
   ],
   controllers: [],
   providers: [],
